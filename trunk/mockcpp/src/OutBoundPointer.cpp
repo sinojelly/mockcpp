fa57a3f7d74facd8cbb1a0ff6b33b1306e9834f3
darwin.yuan <darwin.yuan@9e0d7ab1-db4c-0410-84d4-f5e70a360cb6>
2008-05-31 03:32:33 +0000

    Move more things into cpp

---
+++ trunk/mockcpp/src/OutBoundPointer.cpp
@@ -0,0 +1,27 @@
+
+#include <OutBoundPointer.h>
+#include <Formatter.h>
+#include <Asserter.h>
+
+MOCKCPP_NS_START
+
+void OutBoundPointerCheckConst(const std::string& typeString, bool isConst) 
+{
+   oss_t oss;
+
+   oss << "A constant pointer " << typeString
+       << " cannot be outbounded";
+
+   MOCKCPP_ASSERT_FALSE_MESSAGE( oss.str(), isConst);
+}
+
+OutBoundPointer<void*>::OutBoundPointer(void* p, size_t size, Constraint* constraint)
+	 : OutBoundPointerBase<void*>(p, size, constraint)
+{
+	MOCKCPP_ASSERT_TRUE_MESSAGE(
+            "parameter \"size\" of OutBoundPointer<void*> cannot be specified as 0",
+            size > 0);
+}
+
+MOCKCPP_NS_END
+
