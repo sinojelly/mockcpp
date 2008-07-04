800b3dee9ca5f7a0232360afcc27806f69a57229
darwin.yuan <darwin.yuan@9e0d7ab1-db4c-0410-84d4-f5e70a360cb6>
2008-07-04 19:54:59 +0000

    added support for proc stub

--- mockcpp/include/ProcStub.h
+++ mockcpp/include/ProcStub.h
@@ -7,11 +7,14 @@
 #include <RefAny.h>
 #include <Any.h>
 #include <Invocation.h>
+#include <RepeatMacros.h>
 
 MOCKCPP_NS_START
 
 template <typename F>
 struct ProcStub;
+
+class ProcStubBaseImpl;
 
 struct ProcStubBase : public Stub
 {
@@ -19,55 +22,131 @@ public:
 
     ProcStubBase(const std::string& name, void* addr);
 
+    ~ProcStubBase();
+
+    std::string toString() const;
+    
 private:
 
-    std::string describeSelf() const ;
-
-private:
-
-    std::string procName;
-    void* address;
+    ProcStubBaseImpl* This;
 };
 
+////////////////////////////////////////////////////
 std::string getParameterMismatchString(int n
                 , std::string p
                 , const Invocation& inv);
 
+////////////////////////////////////////////////////
 #define MOCKCPP_CHECK_AND_ASSIGN_PARAMETER(N) \
         MOCKCPP_ASSERT_TRUE_MESSAGE( \
                     getParameterMismatchString(N, TypeString<P##N>::value(), inv) \
-                        , any_castable<P1>(inv.getParameter(N))); \
-        P##N p##N = any_cast<P##N>(inv.getParameter(N))
+                        , any_castable<P##N>(inv.getParameter(N))); \
+        P##N p##N = any_cast<P##N>(inv.getParameter(N));
     
-template <typename R, typename P1>
-struct ProcStub<R(P1)> : public ProcStubBase
-{
-    typedef R (*Func)(P1);
-
-    ProcStub(Func f, std::string name)
-        : ProcStubBase(name, (void*)f), func(f)
+#define PROC_STUB_CONS()  \
+    ProcStub(Func f, std::string name) \
+        : ProcStubBase(name, (void*)f), func(f) \
     {} 
 
-    Any& invoke(const Invocation& inv)
-    {
-		MOCKCPP_CHECK_AND_ASSIGN_PARAMETER(1);
-        result = func(p1);
-        return result;
-    }
+///////////////////////////////////////////////////////////
+#define __ARG(n) P ## n
+#define END_ARGS(n) __ARG(n)
+#define ARGS(n) __ARG(n) ,
 
-private:
-    
-    Func func;
-    Any result;
-};
+#define __PARAM(n) p ## n
+#define END_PARAMS(n) __PARAM(n)
+#define PARAMS(n) __PARAM(n) ,
+
+#define TEMPLATE_ARG(n) typename __ARG(n)
+#define TEMPLATE_ARGS(n) , TEMPLATE_ARG(n)
+#define VOID_TEMPLATE_ARGS(n) typename __ARG(n),
+
+#define DECL_TEMPLATE_ARGS(n) SIMPLE_REPEAT(n, TEMPLATE_ARGS)
+#define DECL_VOID_TEMPLATE_ARGS(n) REPEAT(n, VOID_TEMPLATE_ARGS, TEMPLATE_ARG)
+#define DECL_ARGS(n) REPEAT(n, ARGS, END_ARGS)
+#define DECL_PARAMS(n) REPEAT(n, PARAMS, END_PARAMS)
+
+///////////////////////////////////////////////////////////
+#define PROC_STUB_DEF(n) \
+template <typename R DECL_TEMPLATE_ARGS(n)> \
+struct ProcStub<R(DECL_ARGS(n))> : public ProcStubBase \
+{ \
+    typedef R (*Func)(DECL_ARGS(n)); \
+ \
+    PROC_STUB_CONS() \
+ \
+    Any& invoke(const Invocation& inv) \
+    { \
+        SIMPLE_REPEAT(n, MOCKCPP_CHECK_AND_ASSIGN_PARAMETER); \
+ \
+        result = func(DECL_PARAMS(n)); \
+        return result; \
+    } \
+ \
+private: \
+    Func func; \
+    Any result; \
+}; \
+template <typename R DECL_TEMPLATE_ARGS(n)> \
+Stub* invoke(R(*f)(DECL_ARGS(n)), const char* name = 0) \
+{ \
+    return new ProcStub<R(DECL_ARGS(n))>(f, name?name:""); \
+}
+
+#define VOID_PROC_STUB_DEF(n) \
+template <DECL_VOID_TEMPLATE_ARGS(n)> \
+struct ProcStub<void(DECL_ARGS(n))> : public ProcStubBase \
+{ \
+    typedef void (*Func)(DECL_ARGS(n)); \
+ \
+    PROC_STUB_CONS() \
+ \
+    Any& invoke(const Invocation& inv) \
+    { \
+        SIMPLE_REPEAT(n, MOCKCPP_CHECK_AND_ASSIGN_PARAMETER); \
+ \
+        func(DECL_PARAMS(n)); \
+        return getVoidAny(); \
+    } \
+ \
+private: \
+    Func func; \
+}; 
+
+/////////////////////////////////////////////////////
+VOID_PROC_STUB_DEF(0)
+VOID_PROC_STUB_DEF(1)
+VOID_PROC_STUB_DEF(2)
+VOID_PROC_STUB_DEF(3)
+VOID_PROC_STUB_DEF(4)
+VOID_PROC_STUB_DEF(5)
+VOID_PROC_STUB_DEF(6)
+VOID_PROC_STUB_DEF(7)
+VOID_PROC_STUB_DEF(8)
+VOID_PROC_STUB_DEF(9)
+VOID_PROC_STUB_DEF(10)
+VOID_PROC_STUB_DEF(11)
+VOID_PROC_STUB_DEF(12)
+
+PROC_STUB_DEF(0)
+PROC_STUB_DEF(1)
+PROC_STUB_DEF(2)
+PROC_STUB_DEF(3)
+PROC_STUB_DEF(4)
+PROC_STUB_DEF(5)
+PROC_STUB_DEF(6)
+PROC_STUB_DEF(7)
+PROC_STUB_DEF(8)
+PROC_STUB_DEF(9)
+PROC_STUB_DEF(10)
+PROC_STUB_DEF(11)
+PROC_STUB_DEF(12)
+
+/////////////////////////////////////////////////////
 
 #define PROC(function) function, #function
 
-template <typename RT, typename P1>
-Stub* invoke(RT(*f)(P1), const char* name = 0)
-{
-    return new ProcStub<RT(P1)>(f, name?name:"");
-}
+/////////////////////////////////////////////////////
 
 MOCKCPP_NS_END
 
