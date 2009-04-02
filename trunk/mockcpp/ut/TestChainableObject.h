d6c5a1ec81d8b620f9827e8da38231100e9a5ad6
darwin.yuan <darwin.yuan@9e0d7ab1-db4c-0410-84d4-f5e70a360cb6>
2009-04-02 14:57:25 +0000

    moved all ut testsuites to cxxtest framework

---
+++ trunk/mockcpp/ut/TestChainableObject.h
@@ -0,0 +1,177 @@
+
+#include <cxxtest/TestSuite.h>
+#include <Any.h>
+#include <ChainableMockObject.h>
+#include <Namespace.h>
+#include <InvocationMockBuilder.h>
+#include <ChainingMockHelper.h>
+
+using namespace mockcpp;
+
+
+
+class TestChainableObject : public CxxTest::TestSuite
+{
+	class Foo : public ChainableMockObject
+	{
+	public:
+		Foo(Namespace* ns = 0)
+			: ChainableMockObject("Foo", ns)
+		{}
+
+		int foo(int& i, unsigned long j)
+		{
+			return invoke<int>("foo")("foo", i, j);
+		}
+	};
+public:
+
+	void setUp() { }
+	void tearDown() { }
+
+	/////////////////////////////////////////////////////////
+	void testShouldSupportOrderingExpectationByAfter()
+	{
+		Foo foo;
+
+		foo.method("foo")
+		   .expects(once())
+		   .with(eq(1), eq((unsigned long)2))
+		   .will(returnValue(10))
+		   .id("1");
+
+		foo.method("foo")
+		   .expects(once())
+		   .with(eq(2), eq((unsigned long)2))
+		   .after("1")
+		   .will(returnValue(10));
+
+		int i = 2, j = 1;
+
+		TS_ASSERT_EQUALS(10, foo.foo(j, 2));
+		TS_ASSERT_EQUALS(10, foo.foo(i, 2));
+
+		foo.verify();
+	}
+
+	void testShouldSupportOrderingExpectationByBefore()
+	{
+		Foo foo;
+
+		foo.method("foo")
+		   .expects(once())
+		   .before("1")
+		   .with(eq(2), eq((unsigned long)2))
+		   .will(returnValue(10));
+
+		foo.method("foo")
+		   .expects(once())
+		   .with(eq(1), eq((unsigned long)2))
+		   .will(returnValue(10))
+		   .id("1");
+
+		int i = 2, j = 1;
+
+		TS_ASSERT_EQUALS(10, foo.foo(i, 2));
+		TS_ASSERT_EQUALS(10, foo.foo(j, 2));
+
+		foo.verify();
+	}
+
+	void testShouldFailIfInvocationOutOfOrder()
+	{
+		Foo foo;
+
+		foo.method("foo")
+		   .expects(once())
+		   .with(eq(1), eq((unsigned long)2))
+		   .will(returnValue(10))
+		   .id("1");
+
+		foo.method("foo")
+		   .expects(once())
+		   .before("1")
+		   .with(eq(2), eq((unsigned long)2))
+		   .will(returnValue(10));
+
+		int i = 2, j = 1;
+
+		TS_ASSERT_EQUALS(10, foo.foo(j, 2));
+
+		TS_ASSERT_THROWS(foo.foo(i,2), mockcpp::Exception);
+
+		TS_ASSERT_THROWS(foo.verify(), mockcpp::Exception);
+	}
+
+	void testShouldFailIfInvocationOutOfOrder2()
+	{
+		Foo foo;
+
+		foo.method("foo")
+	   	   .expects(once())
+	   	   .with(eq(1), eq((unsigned long)2))
+		   .will(returnValue(10))
+                   .id("1");
+
+		foo.method("foo")
+		   .expects(once())
+		   .with(eq(2), eq((unsigned long)2))
+		   .after("1")
+		   .will(returnValue(10));
+
+		int i = 2, j = 1;
+
+		TS_ASSERT_THROWS(foo.foo(i, 2), mockcpp::Exception);
+		TS_ASSERT_EQUALS(10, foo.foo(j, 2));
+
+		TS_ASSERT_THROWS(foo.verify(), mockcpp::Exception);
+	}
+
+	void testShouldFailIfReturnedTypeDoesNotMatchRequiredType2()
+	{
+		Foo foo;
+
+		foo.method("foo")
+		   .defaults()
+	  	   .will(returnValue((long)10));
+
+		int i = 2;
+
+		TS_ASSERT_THROWS(foo.foo(i, 2), mockcpp::Exception);
+
+		foo.verify();
+	}
+
+	void testShouldFailIfReturnedTypeDoesNotMatchRequiredType()
+	{
+		Foo foo;
+
+		foo.method("foo")
+		   .expects(once())
+		   .with(eq(1), eq((unsigned long)2))
+		   .will(returnValue((long)10));
+
+		int i = 1;
+
+		TS_ASSERT_THROWS(foo.foo(i,2), mockcpp::Exception);
+
+		foo.verify();
+	}
+
+	void testShouldFailIfInvokedTimesIsMoreThanExpectedTimes()
+	{
+		Foo foo;
+
+		foo.method("foo")
+		   .expects(never())
+		   .with(eq(2), eq((unsigned long)2))
+		   .will(returnValue(10));
+
+		int i = 2;
+
+		TS_ASSERT_THROWS(foo.foo(i,2), mockcpp::Exception);
+
+		foo.verify();
+	}
+};
+
