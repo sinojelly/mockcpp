d30363e546e071082bcd8286a5d413ea72f7b921
darwin.yuan <darwin.yuan@9e0d7ab1-db4c-0410-84d4-f5e70a360cb6>
2009-04-02 10:22:12 +0000

    moved more ut testsuites to framework cxxtest

---
+++ trunk/mockcpp/ut/TestReturnObjectList.h
@@ -0,0 +1,90 @@
+
+#include <cxxtest/TestSuite.h>
+#include <Any.h>
+#include <ChainableMockObject.h>
+#include <Namespace.h>
+#include <InvocationMockBuilder.h>
+#include <ChainingMockHelper.h>
+
+using namespace mockcpp;
+
+class Foo : public ChainableMockObject
+{
+public:
+	Foo(Namespace* ns = 0)
+		: ChainableMockObject("Foo", ns)
+	{}
+
+	int foo(int& i, unsigned long j)
+	{
+		return invoke<int>("foo")("foo",i, j);
+	}
+};
+
+class TestReturnObjectList : public CxxTest::TestSuite
+{
+public:
+
+	void setUp() { }
+	void tearDown() { }
+
+	/////////////////////////////////////////////////////////
+	void testShouldSupportReturnObjectList()
+	{
+		Foo foo;
+
+		foo.method("foo")
+		   .expects(exactly(3))
+		   .with(eq(1), eq((unsigned long)4))
+		   .will(returnObjectList(3, 4, 5));
+
+		int i = 1;
+
+		TS_ASSERT_EQUALS(3, foo.foo(i, 4));
+		TS_ASSERT_EQUALS(4, foo.foo(i, 4));
+		TS_ASSERT_EQUALS(5, foo.foo(i, 4));
+
+		foo.verify();
+	}
+
+	void testShouldThrowExceptionIfTheTypeOfAReturnObjectMismatch()
+	{
+		Foo foo;
+
+		foo.method("foo")
+		   .expects(exactly(3))
+		   .with(eq(1), eq((unsigned long)4))
+		   .will(returnObjectList(3, (long)4, 5));
+
+		int i = 1;
+
+		TS_ASSERT_EQUALS(3, foo.foo(i, 4));
+		TS_ASSERT_THROWS(foo.foo(i,4), mockcpp::Exception);
+		//TS_ASSERT_EQUALS(4, foo.foo(i, 4));
+		TS_ASSERT_EQUALS(5, foo.foo(i, 4));
+
+		foo.verify();
+	}
+
+	void testShouldThrowExceptionIfThereIsNoAvailableObjectsToReturn()
+	{
+		Foo foo;
+
+		foo.method("foo")
+		   .stubs()
+		   .with(eq(1), eq((unsigned long)4))
+		   .will(returnObjectList(3, 4, 5));
+
+		int i = 1;
+
+		TS_ASSERT_EQUALS(3, foo.foo(i, 4));
+		TS_ASSERT_EQUALS(4, foo.foo(i, 4));
+		TS_ASSERT_EQUALS(5, foo.foo(i, 4));
+
+		TS_ASSERT_THROWS(foo.foo(i,4), mockcpp::Exception);
+
+		foo.verify();
+	}
+
+};
+
