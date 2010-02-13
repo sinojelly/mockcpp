bd6e2f967747a0d25fc1fc88379f91785a39a305
darwin.yuan <darwin.yuan@9e0d7ab1-db4c-0410-84d4-f5e70a360cb6>
2010-02-13 06:57:59 +0000

    temp checkin

---
+++ trunk/mockcpp/tests/3rdparty/testngpp/src/runner/tags/EmptyTagsFilter.cpp
@@ -0,0 +1,33 @@
+
+#include <iostream>
+
+#include <testngpp/runner/EmptyTagsFilter.h>
+#include <testngpp/internal/Taggable.h>
+#include <testngpp/runner/InternalError.h>
+
+TESTNGPP_NS_START
+
+/////////////////////////////////////////////
+bool
+EmptyTagsFilter::
+matches(const Taggable* obj) const
+{
+   if(obj == 0)
+   {
+      TESTNGPP_INTERNAL_ERROR(2016);
+   }
+      
+   return obj->numberOfTags() == 0;
+}
+
+/////////////////////////////////////////////
+std::string
+EmptyTagsFilter::
+toString() const
+{
+   return "$";
+}
+
+TESTNGPP_NS_END
+
+
