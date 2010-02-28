fec77a39534d0004359e5828b900b18926b863bd
darwin.yuan <darwin.yuan@9e0d7ab1-db4c-0410-84d4-f5e70a360cb6>
2010-02-28 06:02:27 +0000

    changed cmock a little

--- mockcpp/include/mockcpp/mockable.h
+++ mockcpp/include/mockcpp/mockable.h
@@ -19,7 +19,7 @@
 #ifndef __MOCKCPP_MOCKABLE_H
 #define __MOCKCPP_MOCKABLE_H
 
-#if defined(__cplusplus) && defined(MOCKCPP_USE_MOCKABLE)
+#if defined(__cplusplus) && (defined(MOCKCPP_USE_MOCKABLE) || !defined(_MSC_VER))
 #  include <mockcpp/Functor.h>
 #  include <boost/typeof/typeof.hpp>
 #  define MOCKABLE(function) MOCKCPP_NS::Functor<BOOST_TYPEOF(function)> function(#function, __FUNCTION__)
