a0308d0d79d9fcd538831a071d5bbca6114937d6
sinojelly <sinojelly@9e0d7ab1-db4c-0410-84d4-f5e70a360cb6>
2010-12-02 14:27:27 +0000

    1. add -fPIC option, compile ok on suse linux 10 64bit. 2. process jmp instruction ok.

--- mockcpp/src/LinuxCodeModifier.cpp
+++ mockcpp/src/LinuxCodeModifier.cpp
@@ -43,9 +43,21 @@ bool LinuxCodeModifier::modify(void *dest, void *src, size_t size)
 		return false;
 	}
 
+	#if BUILD_FOR_X86
 	//(void)memcpy(dest, src, size); // something wrong on linux: after memcpy(or 5 single byte copy),  the 4 bytes following jmp, src is 0x07c951b0, but dest is 0x07b851b0. so use unsigned int *, it works ok.
 	*((unsigned char *)dest) = *((unsigned char *)src);
 	*((unsigned long*)((unsigned long)dest + 1)) = *((unsigned long*)((unsigned long)src + 1));
+	#else
+	*((unsigned char *)dest) = *((unsigned char *)src);
+	*((unsigned char *)((unsigned long)dest + 1)) = *((unsigned char *)((unsigned long)src + 1));
+    // after this line, dest+2 is 0x00c90000, not 0, so change it.
+	//*((unsigned int*)((unsigned long)dest + 2)) = *((unsigned int*)((unsigned long)src + 2));
+	*((unsigned char *)((unsigned long)dest + 2)) = *((unsigned char *)((unsigned long)src + 2));
+	*((unsigned char *)((unsigned long)dest + 3)) = *((unsigned char *)((unsigned long)src + 3));
+	*((unsigned char *)((unsigned long)dest + 4)) = *((unsigned char *)((unsigned long)src + 4));
+	*((unsigned char *)((unsigned long)dest + 5)) = *((unsigned char *)((unsigned long)src + 5));
+	*((unsigned long*)((unsigned long)dest + 6)) = *((unsigned long*)((unsigned long)src + 6));
+	#endif
 
 	return true;
 }
