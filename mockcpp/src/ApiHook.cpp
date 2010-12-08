f65ef00ba83858854b04e16440e9c8c7b16e1a96
sinojelly <sinojelly@9e0d7ab1-db4c-0410-84d4-f5e70a360cb6>
2010-12-08 15:50:40 +0000

    added X86 support

--- mockcpp/src/ApiHook.cpp
+++ mockcpp/src/ApiHook.cpp
@@ -21,33 +21,12 @@
 #include <inttypes.h>
 
 #include "JmpOnlyApiHook.h"
+#include "ThunkCode.h"
 #include <mockcpp/ApiHook.h>
 #include <mockcpp/JmpCode.h>
 #include <mockcpp/PageAllocator.h>
 
 MOCKCPP_NS_START
-
-#define ZERO_ADDR 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-
-const unsigned char thunkTemplate[] = 
-{
-    0x48, 0xBF, ZERO_ADDR,   // mov rdi, api
-    0xFF, 0x25, 0x00, 0x00, 0x00, 0x00, ZERO_ADDR // jmp stub
-};
-
-struct ThunkCode
-{
-   static void copyTo(void* thunk, const void* api, const void* stub)
-   {
-      ::memcpy(thunk, thunkTemplate, sizeof(thunkTemplate));
-      *(uintptr_t*)((unsigned char*)thunk+2) = (uintptr_t)api;
-      *(uintptr_t*)((unsigned char*)thunk+16) = (uintptr_t)stub;
-   }
-
-   static size_t size()
-   { return sizeof(thunkTemplate); }
-};
-
 
 ////////////////////////////////////////////////////////
 struct ApiHookImpl
