abb35a611701e6dd643596b15f4e7bb67d85b941
darwin.yuan <darwin.yuan@9e0d7ab1-db4c-0410-84d4-f5e70a360cb6>
2010-12-31 22:11:16 +0000

    check in missing files

--- trunk/mockcpp/src/JmpOnlyApiHook.cpp
+++ trunk/mockcpp/src/JmpOnlyApiHook.cpp
@@ -17,7 +17,6 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ***/
 
-#include <iostream>
 #include <string.h>
 
 #include "JmpOnlyApiHook.h"
--- trunk/mockcpp/src/StringConstraint.cpp
+++ trunk/mockcpp/src/StringConstraint.cpp
@@ -16,8 +16,6 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ***/
 
-#include <iostream>
-
 #include <mockcpp/types/AnyCast.h>
 #include <mockcpp/StringPredict.h>
 #include <mockcpp/StringConstraint.h>
