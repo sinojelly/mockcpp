af3b9f384930234fe82a2184d23e873c74a47ef8
Jelly Chen <sinojelly@163.com>
2021-03-09 23:22:22 +0800

    make mockcpp build_install work on MinGW

--- build_install.ps1
+++ build_install.ps1
@@ -5,12 +5,13 @@
 
 # You can change this testngpp install dir by yourself, it must be an absolute path.
 #$INSTALL_DIR="D:\Projects\Google\cpp-ut-project\cpp-ut-project\tools\mockcpp"
-$INSTALL_DIR="D:\Projects\Google\cpp-project\cpp-project\tools_pdb\mockcpp"
+$INSTALL_DIR="D:\Develop\work\test-tools\mockcpp"
+if (!(test-path $INSTALL_DIR)) { mkdir $INSTALL_DIR }
 
 # add vcbuild path  and cmake path
 # It's better to add to your computer's path environment variable, than to modify $env:path below.
-$env:path=$env:path+";C:\Program Files\Microsoft Visual Studio 9.0\VC\vcpackages"
-$env:path=$env:path+";D:\Tools\CMD\cmake-2.8.1-win32-x86\bin"
+#$env:path=$env:path+";C:\Program Files\Microsoft Visual Studio 9.0\VC\vcpackages"
+#$env:path=$env:path+";D:\Tools\CMD\cmake-2.8.1-win32-x86\bin"
 
 # specify the Visual Studio Version, not set to use default
 $VC_VER="-G `"Visual Studio 9 2008`""
@@ -26,7 +27,18 @@ function build($build_dir, $src_dir) {
 	vcbuild INSTALL.vcproj
 }
 
-build ..\build_mockcpp_to_install ..\mockcpp
+
+$MINGW_VER="-G `"MinGW Makefiles`""
+
+function buildMinGW($build_dir, $src_dir) { 
+	if (!(test-path $build_dir)) { mkdir $build_dir }
+	cd $build_dir
+	Invoke-Expression "cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR $src_dir $MINGW_VER"
+	make install
+}
+
+#build ..\build_mockcpp_to_install ..\mockcpp
+buildMinGW ..\build_mockcpp_to_install ..\mockcpp
 
 cd ..\mockcpp
 
