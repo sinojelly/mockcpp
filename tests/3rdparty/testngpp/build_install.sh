37546f46a2bcfa249ff646bed6a8b7fd0700a248
Chen Guodong <sinojelly@163.com>
2021-03-28 06:22:19 +0000

    move testngpp-install build dir to source root.

--- tests/3rdparty/testngpp/build_install.sh
+++ tests/3rdparty/testngpp/build_install.sh
@@ -2,24 +2,24 @@
 # build and install
 
 # You can change this testngpp install dir by yourself, it must be an absolute path.
-install_dir=/home/jelly/Programming/testngpp/install/testngpp
+install_dir=~/test_tools/testngpp_install
 
 
 function build() {
-	mkdir $1 2>/dev/null
+	mkdir -p $1 2>/dev/null
 	cd $1
 	cmake -DCMAKE_INSTALL_PREFIX=$install_dir $2
 	make install
 }
 
-build ../../../../build_testngpp_to_install ../mockcpp/tests/3rdparty/testngpp
+build ../../../build/testngpp_to_install ../../tests/3rdparty/testngpp
 
-cd ../mockcpp/tests/3rdparty/testngpp
+cd ../../tests/3rdparty/testngpp
 
-mv -f -u $install_dir/3rdparty $install_dir/..
+#mv -f -u $install_dir/3rdparty $install_dir/..
 
-cd scripts
-python setup_linux.py build
-cp build/exe.linux-i686-2.6/* $install_dir/bin
+#cd scripts
+#python setup_linux.py build
+#cp build/exe.linux-i686-2.6/* $install_dir/bin
 
-cd ..
+#cd ..
