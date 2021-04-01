ecc7e2cfa596f22344d8ebb0faebc3d99dde8f23
Chen Guodong <sinojelly@163.com>
2021-04-01 22:45:31 +0800

    refactor testngpp build_install.ps1, not install boost

--- tests/3rdparty/testngpp/build_install.ps1
+++ tests/3rdparty/testngpp/build_install.ps1
@@ -3,52 +3,54 @@
 # If you use powershell the first time, excute this cmd first: set-executionpolicy remotesigned
 # Build mockcpp and it's tests, and at last run all tests.
 
-# You can change this testngpp install dir by yourself, it must be an absolute path.
-#$TESTNGPP_INSTALL_DIR="D:\Projects\Google\cpp-ut-project\cpp-ut-project\tools\testngpp"
-$TESTNGPP_INSTALL_DIR="D:\Develop\work\test-tools\testngpp"
-if (!(test-path $TESTNGPP_INSTALL_DIR)) { mkdir $TESTNGPP_INSTALL_DIR }
+# $args[0]  --- compiler index in table $SUPPORTED_COMPILER
+# $args[2]  --- install dir, it's an absolute path
 
-# add vcbuild path  and cmake path
-# It's better to add to your computer's path environment variable, than to modify $env:path below.
-#$env:path=$env:path+";C:\Program Files\Microsoft Visual Studio 9.0\VC\vcpackages"
-#$env:path=$env:path+";D:\Tools\CMD\cmake-2.8.1-win32-x86\bin"
-$env:path=$env:path+";$PSScriptRoot"  # to find premake4.exe
+. "$PSScriptRoot\..\..\..\tools\build_functions.ps1"
 
-# specify the Visual Studio Version, not set to use default
-$VC_VER="-G `"Visual Studio 9 2008`""
-#$VC_VER="-G `"Visual Studio 10`""
+# Use the following content to make a CMakeLists.txt to get compiler name and major_ver, run : cmake -S . -B build
+# CMAKE_MINIMUM_REQUIRED(VERSION 3.12...3.19)
+# PROJECT(cmake-test)
+# MESSAGE("CMAKE_CXX_COMPILER_ID - compiler name:")
+# MESSAGE(${CMAKE_CXX_COMPILER_ID})
+# MESSAGE("CMAKE_CXX_COMPILER_VERSION - compiler major version:")
+# MESSAGE(${CMAKE_CXX_COMPILER_VERSION})
+#
+# cmake_param also can be: "-G `"Visual Studio 16 2019`" -A Win32"  etc.
+$SUPPORTED_COMPILER = @(
+           @{ name="GNU"; major_ver="7"; cmake_param="-G `"MinGW Makefiles`""},  # 0 --- MinGW GNU 7.x
+           @{ name="GNU"; major_ver="8"; cmake_param="-G `"MinGW Makefiles`""},  # 1 --- MinGW GNU 8.x
+           @{ name="MSVC"; major_ver="19"; cmake_param="-G `"Visual Studio 16 2019`"" }  # 2 --- Visual Studio 2019
+          )
 
-# build Debug only
-$env:VCBUILD_DEFAULT_CFG="Debug|Win32"
-
-function build($build_dir, $src_dir) { 
-	if (!(test-path $build_dir)) { mkdir $build_dir }
-	cd $build_dir
-	Invoke-Expression "cmake -DCMAKE_INSTALL_PREFIX=$TESTNGPP_INSTALL_DIR $src_dir $VC_VER"
-	ls *.sln -name | vcbuild
-	vcbuild INSTALL.vcproj
+# If no input parameter
+if (-not $args[0]) {
+    $DEFAULT_COMPILER_INDEX=2   # default choose vs2019
+} else {
+    $DEFAULT_COMPILER_INDEX=$args[0] 
 }
 
-$MINGW_VER="-G `"MinGW Makefiles`""
-
-function buildMinGW($build_dir, $src_dir) { 
-	if (!(test-path $build_dir)) { mkdir $build_dir }
-	cd $build_dir
-	Invoke-Expression "cmake -DCMAKE_INSTALL_PREFIX=$TESTNGPP_INSTALL_DIR $src_dir $MINGW_VER"
-	make install
+if (-not $args[1]) {
+    $DEFAULT_INSTALL_PATH="D:\Develop\test-tools\testngpp-install"   # should be absolute path
+} else {
+    $DEFAULT_INSTALL_PATH=$args[1] 
 }
 
-#build ..\..\..\..\build_testngpp_to_install ..\mockcpp\tests\3rdparty\testngpp
-buildMinGW ..\..\..\..\build_testngpp_to_install ..\mockcpp\tests\3rdparty\testngpp
+if (!(test-path $DEFAULT_INSTALL_PATH)) { mkdir $DEFAULT_INSTALL_PATH }
 
-cd ..\mockcpp\tests\3rdparty\testngpp
+$global:MY_OS_NAME=$null
+$global:MY_CXX_COMPILER_NAME=$null
+$global:MY_CXX_COMPILER_MAJOR_VERSION=$null
+$global:CMAKE_COMPILER_PARAM=$null
+$global:MAKE_BUILD_TYPE=$null
 
-#move $TESTNGPP_INSTALL_DIR\3rdparty $TESTNGPP_INSTALL_DIR\.. -force
+InitEnviroment $SUPPORTED_COMPILER[$DEFAULT_COMPILER_INDEX].name $SUPPORTED_COMPILER[$DEFAULT_COMPILER_INDEX].major_ver $SUPPORTED_COMPILER[$DEFAULT_COMPILER_INDEX].cmake_param
 
-# On Windows, to avoid install python, you can make the python scripts to be an .exe, you should install py2exe first.(www.py2exe.org)
-# cd testngpp\scripts, run premake4 install, then copy testngppgen.exe to tools\testngpp\bin\ dir.
-cd scripts
-Invoke-Expression "premake4 install"
-cp dist\testngppgen.exe $TESTNGPP_INSTALL_DIR\bin
+$BUILD_DIR="build_$global:MY_CXX_COMPILER_NAME"
 
-cd ..
+$OS_COMPILER="$global:MY_OS_NAME\$global:MY_CXX_COMPILER_NAME\$global:MY_CXX_COMPILER_MAJOR_VERSION"
+
+echo "OS_COMPILER in Powershell : $OS_COMPILER"
+
+Invoke-Expression "cmake $global:CMAKE_COMPILER_PARAM -S . -B $BUILD_DIR/testngpp-install -DCMAKE_INSTALL_PREFIX=$DEFAULT_INSTALL_PATH"
+CompileProjectInstall $global:MY_CXX_COMPILER_NAME $BUILD_DIR/testngpp-install
