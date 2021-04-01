ac50561fd72d362bfbd3cadc5423f70a95ee1b0d
Chen Guodong <sinojelly@163.com>
2021-04-02 00:02:53 +0800

    refactor mockcpp build_install.sh

--- build_install.sh
+++ build_install.sh
@@ -1,16 +1,46 @@
 #!/bin/bash
 # build and install
 
-install_dir=~/test_tools/mockcpp_install
+# $1  --- compiler name  (GNU)
+# $2  --- [optional] compiler major version  (The first part of cxx compiler version)
 
-function build() {
-	mkdir -p $1 2>/dev/null
-	cd $1
-	cmake -DCMAKE_INSTALL_PREFIX=$install_dir $2
-	make install
-}
+# fast fail
+set -e
 
-build build/mockcpp_to_install ../..
+INSTALL_PATH=~/test_tools/mockcpp_install
 
-cd ../..
+# Set the xxunit test framework name
+XUNIT_NAME=testngpp
+#XUNIT_NAME=gtest
+#XUNIT_NAME=catch2
+
+# gtest: there is a $XUNIT_HOME/include/gtest/gtest.h
+# catch2: there is a $XUNIT_HOME/include/catch2/catch_test_macros.hpp
+#XUNIT_HOME=/usr/local
+
+. "tools/build_functions.sh"
+
+MY_OS_NAME=""
+MY_CXX_COMPILER_NAME=""
+MY_CXX_COMPILER_MAJOR_VERSION=""
+CMAKE_COMPILER_PARAM=""
+MAKE_BUILD_TYPE=""
+
+AUTO_COMPILER="GNU"  #  $1
+AUTO_CXX_VER=`gcc -dumpversion | awk -F.  '{print $1}'`  # $2
+InitEnviroment $AUTO_COMPILER $AUTO_CXX_VER
+
+BUILD_DIR="build_$MY_CXX_COMPILER_NAME"
+
+OS_COMPILER="$MY_OS_NAME/$MY_CXX_COMPILER_NAME/$MY_CXX_COMPILER_MAJOR_VERSION"
+
+echo "OS_COMPILER in Shell : $OS_COMPILER"
+
+if [ "$XUNIT_NAME" == "testngpp" ];then
+cmake  -S . -B $BUILD_DIR/mockcpp-install4$XUNIT_NAME -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH
+else
+cmake  -S . -B $BUILD_DIR/mockcpp-install4$XUNIT_NAME -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH -DMOCKCPP_XUNIT=$XUNIT_NAME -DMOCKCPP_XUNIT_HOME=$XUNIT_HOME
+fi
+
+CompileProjectInstall $MY_CXX_COMPILER_NAME $BUILD_DIR/mockcpp-install4$XUNIT_NAME
 
