9f5f4d3633a94d37390a25091c3768594d924fc2
Zhao Yonggang <yonggang.zhao@icloud.com>
2021-07-17 12:04:44 +0800

    Keep format consistent with project

--- tests/ut/TestCheck.h
+++ tests/ut/TestCheck.h
@@ -159,8 +159,8 @@ FIXTURE(Check)
 		MOCK_METHOD(mocker, method)
 			.expects(once())
 			.with(checkWith([](STRUCT_T *p) {
-			    p->a = 10;
-				return p->b == 2;
+                p->a = 10;
+                return p->b == 2;
 			}));
 
 		client(mocker, &input);
