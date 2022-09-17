5cf1dc56213c39fe5c6b2d774651f780bad71f72
sunjianping <ifindex@163.com>
2022-09-17 14:55:43 +0800

    feat: support mocker running in multi-thread environment

--- include/mockcpp/ApiHookFunctor.h
+++ include/mockcpp/ApiHookFunctor.h
@@ -18,6 +18,7 @@
 #ifndef __MOCKCPP_API_HOOK_FUNCTOR_H__
 #define __MOCKCPP_API_HOOK_FUNCTOR_H__
 
+#include <mutex>
 #include <mockcpp/mockcpp.h>
 #include <mockcpp/GlobalMockObject.h>
 
@@ -39,8 +40,11 @@ private: \
  \
    static R CallingConvention hook(DECL_PARAMS_LIST(n)) \
    { \
-      return GlobalMockObject::instance.invoke<R>(apiAddress) \
+      mtx.lock(); \
+      R ret = GlobalMockObject::instance.invoke<R>(apiAddress) \
                                 (empty_caller DECL_REST_PARAMS(n)); \
+      mtx.unlock(); \
+      return ret; \
    } \
  \
    static bool appliedBy(F* api) \
@@ -77,23 +81,82 @@ public: \
 private: \
    static void* apiAddress; \
    static unsigned int refCount; \
+   static std::mutex mtx; \
 }; \
 template <typename R DECL_TEMPLATE_ARGS(n), unsigned int Seq> \
 void* ApiHookFunctor<R CallingConvention (DECL_ARGS(n)), Seq>::apiAddress = 0; \
 template <typename R DECL_TEMPLATE_ARGS(n), unsigned int Seq> \
-unsigned int ApiHookFunctor<R CallingConvention (DECL_ARGS(n)), Seq>::refCount = 0 
+std::mutex ApiHookFunctor<R CallingConvention (DECL_ARGS(n)), Seq>::mtx; \
+template <typename R DECL_TEMPLATE_ARGS(n), unsigned int Seq> \
+unsigned int ApiHookFunctor<R CallingConvention (DECL_ARGS(n)), Seq>::refCount = 0; \
+template <DECL_TEMPLATE_ARGS_RET_VOID(n) unsigned int Seq> \
+struct ApiHookFunctor<void CallingConvention (DECL_ARGS(n)), Seq> \
+{ \
+private: \
+   typedef void CallingConvention F (DECL_ARGS(n)); \
+ \
+   static void CallingConvention hook(DECL_PARAMS_LIST(n)) \
+   { \
+      mtx.lock(); \
+      GlobalMockObject::instance.invoke<void>(apiAddress) \
+                                (empty_caller DECL_REST_PARAMS(n)); \
+      mtx.unlock(); \
+   } \
+ \
+   static bool appliedBy(F* api) \
+   { return apiAddress == reinterpret_cast<void*>(api); } \
+ \
+   static void* getHook() \
+   { return reinterpret_cast<void*>(hook); } \
+ \
+   static void freeHook() \
+   { if(--refCount == 0) apiAddress = 0; } \
+public: \
+ \
+   static void* getApiHook(F* api) \
+   { \
+      if(! appliedBy(api)) return 0; \
+      ++refCount; \
+      return getHook(); \
+   } \
+ \
+   static void* applyApiHook(F* api) \
+   { \
+      if(apiAddress != 0) return 0; \
+      apiAddress = reinterpret_cast<void*>(api); \
+      refCount = 1; \
+      return getHook(); \
+   } \
+ \
+   static bool freeApiHook(void* hook) \
+   { \
+       if(getHook() != hook) return false; \
+       freeHook(); \
+       return true; \
+   } \
+private: \
+   static void* apiAddress; \
+   static unsigned int refCount; \
+   static std::mutex mtx; \
+}; \
+template <DECL_TEMPLATE_ARGS_RET_VOID(n) unsigned int Seq> \
+void* ApiHookFunctor<void CallingConvention (DECL_ARGS(n)), Seq>::apiAddress = 0; \
+template <DECL_TEMPLATE_ARGS_RET_VOID(n) unsigned int Seq> \
+std::mutex ApiHookFunctor<void CallingConvention (DECL_ARGS(n)), Seq>::mtx; \
+template <DECL_TEMPLATE_ARGS_RET_VOID(n) unsigned int Seq> \
+unsigned int ApiHookFunctor<void CallingConvention (DECL_ARGS(n)), Seq>::refCount = 0
 
 #if defined(_MSC_VER)
 // TODO: ApiHook related tests failed on VS2019.
 // [  ERROR   ] TestApiHook.h:66: hardware exception STATUS_ILLEGAL_INSTRUCTION raised in setup or running test
 // [  ERROR   ] TestApiHook.h:66 : hardware exception STATUS_ACCESS_VIOLATION raised in teardown
-#if _MSC_VER >= 1920    // VS 2019 
+#if _MSC_VER >= 1920    // VS 2019
 #define MOCKCPP_API_HOOK_FUNCTOR_DEF(n) \
 __MOCKCPP_API_HOOK_FUNCTOR_DEF(n, __stdcall)
 #else
 #define MOCKCPP_API_HOOK_FUNCTOR_DEF(n) \
 __MOCKCPP_API_HOOK_FUNCTOR_DEF(n, ); \
-__MOCKCPP_API_HOOK_FUNCTOR_DEF(n, __stdcall) 
+__MOCKCPP_API_HOOK_FUNCTOR_DEF(n, __stdcall)
 #endif
 #else
 #define MOCKCPP_API_HOOK_FUNCTOR_DEF(n) \
